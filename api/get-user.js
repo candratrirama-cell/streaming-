404c09c3ee7e411f4510d5dff88affff1988da47
candratrirama-cell <candratrirama@gmail.com>
2026-04-04 16:44:55 +0700

    Delete api directory

--- api/get-user.js
+++
@@ -1,8 +0,0 @@
-import admin from 'firebase-admin';
-// (Inisialisasi Admin SDK)
-
-export default async function handler(req, res) {
-    const { uid } = req.query;
-    const snapshot = await admin.database().ref(`users/${uid}`).once('value');
-    res.status(200).json(snapshot.val());
-}
